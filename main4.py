2753c47d7b7ebdc547b803c75732447af0e4497c
Matías Aliaga <maaliaga234@uc.cl>
2023-10-31 16:25:54 -0300

    feat: Código actualizado con cálculo de costo de TENS, obteniendo costo de enfermeras + tens

--- main4.py
+++ main4.py
@@ -92,6 +92,79 @@ def simular_mes_enfermeras(mes, cant_personal_extra):
     #print(f"\nCostos acumulados de {costos_acumulados} para el mes {mes}")
     return costos_acumulados
 
+def simular_mes_tens(mes, cant_personal_extra):
+    dias = calendar.monthrange(2023, mes)[1]
+    #Esto se va a retornar
+    costos_acumulados = costo_tens["costo_fijo_on_demand"]*sum(cant_personal_extra.values())
+    # i = fila/enfermera, dia = columna
+    tabla1 = generador_tablas(dias, parametros["num_tens"])
+    # Este proceso revisa cada dia si la enfermera va a trabajar o no (si es que le corresponde)
+    for area in parametros["areas"]:
+        tabla_indefinido = generador_tablas(dias, cant_tens_por_area[area])
+        tabla_extra_indefinido = generar_planilla_horas_extras(tabla_indefinido)
+        tabla_on_demand = generar_tabla_personal_extra(tabla1, cant_personal_extra[area])
+        alpha = alpha_meses_tens[area][mes]
+        beta = dias-alpha
+        for dia in range(len(tabla_indefinido[0])):
+            # Este proceso decide si la enfermera con contrato indefinido que le toca ir va o no
+            for i in range(len(tabla_indefinido)):
+                tens = tabla_indefinido[i][dia]
+                if tens == "D" or tens == "N":
+                    if random.random() <= bernoulli_meses_tens[area][mes]:
+                        # Aquí hay que implementar la distribución discreta para la cantidad de días
+                        # Esto requiere ser modificado y pulido
+                        dias_ausente = math.ceil(dias*random.betavariate(alpha, beta))
+                        #print(f"La enfermera de índice {i} se ha ausentado por {dias_ausente} días")
+                        for j in range(0, dias_ausente):
+                            try:
+                            #Esto representa un ausentismo
+                                tabla_indefinido[i][dia+j] = "A"
+                            except:
+                                continue
+                    else:
+                        #print(f"la enfermera {i} asistió al trabajo el dia {dia}" )
+                        costos_acumulados += costo_enfermeras["costo_hora_indefinido"]
+                    
+
+        # Este proceso debiese revisar que se cumplan las restricciones:
+        # En particular las restricciones a llenar son que se cubra el ratio pero queda por implementar:
+        # Por el momento que se satisfaga que cumpla que cada columna tenga al menos un dia y una noche 
+        tabla_np = np.array(tabla1)
+        # Count the number of "N" values in the third column (column index 2)
+        column_to_count = tabla_np[:, dia]  # Extract the third column
+        count_of_N = np.count_nonzero(column_to_count == 'N')  # Count "N" values in the column
+        count_of_D = np.count_nonzero(column_to_count == 'D')
+        
+        # Si no hay suficientes enfermeras para cubrir el ratio:
+        if count_of_N < min_tens_por_area[area]:
+            faltante_N = min_tens_por_area[area]-count_of_N
+            # Aca hay que hacer algo xd, seleccionar potenciales horas extras. Implementar los algoritmos!
+            #print(tabla_horas_extra)
+            #print(implementar_personal_extra(tabla_personal_extra, "N", dia))
+            if tabla_extra_indefinido == implementar_turnos_extras(tabla_indefinido, tabla_extra_indefinido, "N", dia):
+                costos_acumulados += costo_tens["costo_hora_indefinido"]*costo_tens["factor_hora_extra_noche"]*faltante_N
+            else:
+                if tabla_on_demand == implementar_personal_extra(tabla_on_demand, "N", dia):
+                    costos_acumulados += parametros["infactibilidad"]
+                    #print(f"Hoy, día {dia} no hay suficientes enfermeras para cubrir la noche")
+                else:
+                    costos_acumulados += costo_tens["costo_hora_on_demand"]*costo_tens["factor_hora_extra_noche"]*faltante_N
+
+        if count_of_D < min_tens_por_area[area]:
+            faltante_D = min_tens_por_area[area]-count_of_D
+            if tabla_extra_indefinido == implementar_turnos_extras(tabla_indefinido, tabla_extra_indefinido, "D", dia):
+                costos_acumulados += costo_tens["costo_hora_indefinido"]*costo_tens["factor_hora_extra_dia"]*faltante_D
+            else:
+                if tabla_on_demand == implementar_personal_extra(tabla_on_demand, "D", dia):
+                    costos_acumulados += parametros["infactibilidad"]
+                    #print(f"Hoy, día {dia} no hay suficientes enfermeras para cubrir la noche")
+                else:
+                    costos_acumulados += costo_tens["costo_hora_on_demand"]*costo_tens["factor_hora_extra_dia"]*faltante_D
+
+    #print(calcular_estadisticas(tabla1, tabla_horas_extra))
+    #print(f"\nCostos acumulados de {costos_acumulados} para el mes {mes}")
+    return costos_acumulados
+
 #simular_mes(1, 4)
 # La lista contiene la cantidad optima de personal extra a contratar cada mes:
 # Como se supone que las distribuciones son variables estas cantidades van a ser variables!
--- optuna_sim.py
+++ optuna_sim.py
@@ -9,7 +9,7 @@ import optuna.visualization as ov
 from optuna.samplers import TPESampler
 
 #Optuna
-def objetivo(trial):
+def objetivo_enfermeras(trial):
     # Define los hiperparámetros que Optuna debe optimizar.
     # En este caso, estamos optimizando los valores en la lista.
     horas_extra_optimas = []
@@ -29,22 +29,69 @@ def objetivo(trial):
     #Queremos minimizar el costo anual.
     return costo_acumulado_anual
 
+def objetivo_tens(trial):
+    # Define los hiperparámetros que Optuna debe optimizar.
+    # En este caso, estamos optimizando los valores en la lista.
+    horas_extra_optimas = []
+    for mes in range(1, 13):
+        horas_extra_mes = {}
+        for area, cant_tens in cant_tens_por_area.items():
+            sugerencia = trial.suggest_int(f'Horas extra mes {mes} {area}', 0, cant_tens)
+            #diccionario de horas extra por mes y area
+            horas_extra_mes[area] = sugerencia
+        horas_extra_optimas.append(horas_extra_mes)
+    
+    # Llama a la función de simulación del año con la lista optimizada.
+    costo_acumulado_anual = 0
+    for mes in range(1, 13):
+        costo_acumulado_anual += simular_mes_tens(mes, horas_extra_optimas[mes-1])
+
+    #Queremos minimizar el costo anual.
+    return costo_acumulado_anual
+
 #costo_anual = simular_año(lista)
 #print("Costo anual: ", costo_anual)
 
+n_iter = 100
+costo_e = 0
+costo_t = 0
+lista_e = []
+lista_t = []
 
 study = optuna.create_study(sampler=TPESampler(), direction='minimize')
-study.optimize(objetivo, n_trials=500, n_jobs=10)
+for i in range(2):
+    if i == 0:
+        study.optimize(objetivo_enfermeras, n_trials=n_iter, n_jobs=10)
 
-# Obtiene los mejores hiperparámetros y su costo asociado.
-best_params = study.best_params
-best_cost  = study.best_value
+        # Obtiene los mejores hiperparámetros y su costo asociado.
+        best_params = study.best_params
+        best_cost  = study.best_value
+        costo_e += best_cost
+        lista_e = [valor for valor in best_params.values()]
 
-print("Mejores hiperparámetros encontrados:")
+        # Visualización en gráficos (creo que se necesita libreria plotly)
+        #ov.plot_optimization_history(study).show(renderer="browser")
+        #ov.plot_param_importances(study).show(renderer="browser") # Hay muchos más pero estos eran los más importantes
+    else:
+        study.optimize(objetivo_tens, n_trials=n_iter, n_jobs=10)
+
+        # Obtiene los mejores hiperparámetros y su costo asociado.
+        best_params = study.best_params
+        best_cost  = study.best_value
+        costo_t += best_cost
+        lista_t = [valor for valor in best_params.values()]
+
+        # Visualización en gráficos (creo que se necesita libreria plotly)
+        #ov.plot_optimization_history(study).show(renderer="browser")
+
+print(f"\nMejores hiperparámetros encontrados ENFERMERAS\n:")
 #Mejor lista de personal extra
-print(f"Mejor lista encontrada: {[valor for valor in best_params.values()]}")
-print(f"Costo mínimo encontrado: {best_cost}")
+print(f"Mejor lista encontrada: {lista_e}")
+print(f"Costo mínimo encontrado: {costo_e}\n")
 
-# Visualización en gráficos (creo que se necesita libreria plotly)
-ov.plot_optimization_history(study).show(renderer="browser")
-#ov.plot_param_importances(study).show(renderer="browser") # Hay muchos más pero estos eran los más importantes
+print("Mejores hiperparámetros encontrados TENS\n:")
+#Mejor lista de personal extra
+print(f"Mejor lista encontrada: {lista_t}")
+print(f"Costo mínimo encontrado: {costo_t}\n")
+
+print(f"COSTO TOTAL: {costo_e+costo_t}")
